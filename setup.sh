1be283a01704e8bd8d583694fdefcef445188416
zarlo <5899@zarlo.dev>
2022-11-12 03:57:53 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/CosmosOS/Cosmos.git --depth=1 --branch=crossplatform
-git clone https://github.com/CosmosOS/IL2CPU.git --depth=1 --branch=crossplatform
+git clone https://github.com/CosmosOS/Cosmos.git --depth=1 --branch=master
+git clone https://github.com/CosmosOS/IL2CPU.git --depth=1 --branch=master
 git clone https://github.com/CosmosOS/XSharp.git --depth=1 --branch=master
 git clone https://github.com/CosmosOS/Common.git --depth=1 --branch=master
